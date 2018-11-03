26559e2821289433e49f134ec8c07a74e3558f38
Andreas <s130016@student.dtu.dk>
2018-11-03 05:50:12 +0100

    TestAfLogiskTabel, Kan nu finde en streng i tabellen.

--- src/Udsagnslogik.java
+++ src/Udsagnslogik.java
@@ -1,40 +1,57 @@
 public class Udsagnslogik {
 
 
-    public void Udsagnslogik(String...String){
+    public void Udsagnslogik(String... String) {
 
-        Udsagnslogik r =new Udsagnsogik;
+        //Udsagnslogik r =new Udsagnsogik;
 
 
     }
 
 
+    public static String[][] logiskIdentitet() {
 
-    public String[][] logiskIdentitet() {
+        String[][] logiskIdentitetListe = new String[20][2];
 
-        String [][] logiskIdentitetListe=new String[20][2];
-
-        logiskIdentitetListe[0][0]="P%Q";
-        logiskIdentitetListe[0][1]="Q%P";
-        logiskIdentitetListe[1][0]="P&Q";
-        logiskIdentitetListe[1][1]="Q&P";
-        logiskIdentitetListe[2][0]="P%(Q%R)";
-        logiskIdentitetListe[2][1]="(P%Q)%R";
-        logiskIdentitetListe[3][0]="P&(Q&R)";
-        logiskIdentitetListe[3][1]="(P&Q)&R";
-        logiskIdentitetListe[4][0]="P&(Q%R)";
-        logiskIdentitetListe[4][1]="(P&Q)%(P&R)";
-        logiskIdentitetListe[5][0]="P%(Q&R)";
-        logiskIdentitetListe[5][1]="(P%Q)&(P%R)";
-        logiskIdentitetListe[6][0]="P%0";
-        logiskIdentitetListe[6][1]="P";
-        logiskIdentitetListe[7][0]="P&1";
-        logiskIdentitetListe[7][1]="P";
-        logiskIdentitetListe[8][0]="P%-P";
-        logiskIdentitetListe[8][1]="1";
-        logiskIdentitetListe[9][0]="P&-1";
-        logiskIdentitetListe[9][1]="0";
+        logiskIdentitetListe[0][0] = "P%Q";
+        logiskIdentitetListe[0][1] = "Q%P";
+        logiskIdentitetListe[1][0] = "P&Q";
+        logiskIdentitetListe[1][1] = "Q&P";
+        logiskIdentitetListe[2][0] = "P%(Q%R)";
+        logiskIdentitetListe[2][1] = "(P%Q)%R";
+        logiskIdentitetListe[3][0] = "P&(Q&R)";
+        logiskIdentitetListe[3][1] = "(P&Q)&R";
+        logiskIdentitetListe[4][0] = "P&(Q%R)";
+        logiskIdentitetListe[4][1] = "(P&Q)%(P&R)";
+        logiskIdentitetListe[5][0] = "P%(Q&R)";
+        logiskIdentitetListe[5][1] = "(P%Q)&(P%R)";
+        logiskIdentitetListe[6][0] = "P%0";
+        logiskIdentitetListe[6][1] = "P";
+        logiskIdentitetListe[7][0] = "P&1";
+        logiskIdentitetListe[7][1] = "P";
+        logiskIdentitetListe[8][0] = "P%-P";
+        logiskIdentitetListe[8][1] = "1";
+        logiskIdentitetListe[9][0] = "P&-1";
+        logiskIdentitetListe[9][1] = "0";
 
         return logiskIdentitetListe;
+    }
+
+    public static void main(String[] args) {
+
+        String r;
+        String s = "P";
+
+        for (int row = 0; row < logiskIdentitet().length; row++)
+            for (int colomn = 0; colomn < logiskIdentitet()[row].length; colomn++) {
+                String w = logiskIdentitet()[row][colomn];
+                if (s.equals(w)) {
+
+                    System.out.println("YES, " + s + "=" + w);
+                    System.out.println(logiskIdentitet()[row][colomn]);
+                    System.out.println(row+"and"+colomn);
+                }
+
+            }
     }
 }
